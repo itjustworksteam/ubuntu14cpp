56da2b16798c47fc97b52564953a78759480b26c
Riccardo Crippa <therickys93@gmail.com>
2017-03-16 11:23:23 +0100

    add google test library

--- install.sh
+++ install.sh
@@ -3,3 +3,8 @@
 sudo apt-get clean
 sudo apt-get update
 sudo apt-get install clang -y
+sudo apt-get install cmake libgtest-dev -y
+cd /usr/src/gtest
+sudo cmake CMakeLists.txt
+sudo make
+sudo cp *.a /usr/lib
